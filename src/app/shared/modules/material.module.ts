21d08f1f91773ce5a0c1be01b26c6584f9d21be7
Korobk Vitaly <korobk.vitaly@gmail.com>
2023-05-30 09:05:46 +0000

    Merged PR 56: added svg icon register

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { MatInputModule } from "@angular/material/input";
 import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 import { MatSelectModule } from "@angular/material/select";
@@ -14,6 +14,8 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatAutocompleteModule } from "@angular/material/autocomplete";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatOptionModule } from "@angular/material/core";
+import { MatStepperModule } from "@angular/material/stepper";
+import { DomSanitizer } from "@angular/platform-browser";
 
 const MATERIAL_MODULES = [
   MatToolbarModule,
@@ -33,7 +35,6 @@ const MATERIAL_MODULES = [
 ]
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
     ...MATERIAL_MODULES,
@@ -42,4 +43,15 @@ const MATERIAL_MODULES = [
     ...MATERIAL_MODULES,
   ]
 })
-export class MaterialModule {}
+export class MaterialModule {
+  constructor(
+    private readonly matIconRegistry: MatIconRegistry,
+    private readonly domSanitizer: DomSanitizer,
+  ){
+    this.matIconRegistry.addSvgIcon(
+      'excel',
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/icons/excel.svg"),
+    );
+
+  }
+}
