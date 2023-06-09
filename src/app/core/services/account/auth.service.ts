d4863bdc4a134e02891013ca7dc427bd96b1fd5e
Taras Kravets <taras.kravets.ua@gmail.com>
2023-06-09 15:35:20 +0300

    Fixes registration method

--- src/app/core/services/account/auth.service.ts
+++ src/app/core/services/account/auth.service.ts
@@ -38,6 +38,7 @@ export class AuthService {
 
   registration(body: AuthRegistration): Observable<AuthUser> {
     return this.http.post<AuthUser>(`${this.env.apiUrlAccount}/user/register`, body).pipe(
+      tap(user => this.setToken(user.jwtToken)),
       switchMap((authUser: AuthUser) => this.setCompanyId(authUser.userId).pipe(
         switchMap(({ companyId }) => {
           const user = { ...authUser, companyId }
